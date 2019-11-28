092c99fb469deab1d0d4bf0bd0497a10e354a3d8
loganthayer1 <logan.thayer@gmail.com>
2019-11-28 18:30:01 -0500

    Logan Thayer Diane Lee ready for submission ex9

--- ex09_pt2.R
+++ ex09_pt2.R
@@ -3,31 +3,8 @@
 #and a scatter plot of all obs (color coded?)
 
 allData <- read.table("data.txt", sep = ",", header = TRUE)
-NorthData <- allData[allData$region=="north",]
-SouthData <- allData[allData$region=="south",]
-EastData <- allData[allData$region=="east",]
-WestData <- allData[allData$region=="west",]
-
-NorthObs <- NorthData$observations
-SouthObs <- SouthData$observations
-EastObs <- EastData$observations
-WestObs <- WestData$observations
-
-df_obs <- data.frame(NorthObs,SouthObs,EastObs,WestObs)
-colnames(df_obs) <- c('North','South','East','West')
-
-df_means <- data.frame(mean(NorthObs),mean(SouthObs),mean(EastObs),mean(WestObs))
-colnames(df_means) <- c('North','South','East','West')
-
-
 library(ggplot2)
 
-
-d <- ggplot(data = allData, aes(x = as.character(region), y = observations))
-d + geom_bar(stat = "identity") + theme_classic() + 
-  xlab("Region") + ylab("Observations")
-
-#DL additions 
 #barplot of population means
 d = ggplot(allData, aes(x = as.character(region), y = observations))
 d  + theme_classic() + stat_summary(geom="bar", fun.y="mean") + 
@@ -36,4 +13,15 @@ d  + theme_classic() + stat_summary(geom="bar", fun.y="mean") +
 #scatterplot
 d = ggplot(allData, aes(x = as.character(region), y = observations))
 d  + theme_classic() + stat_summary(geom="point") + 
-  xlab("Region") + ylab("Observations") + geom_jitter()
+  xlab("Region") + 
+  ylab("Observations") + 
+  geom_jitter()
+
+
+
+#The bar and scatter plots tell a very different story about the data set here.
+#Whereas the bar graph appears to imply that the data sets are very very similar, 
+#the scatter plot displays a vast difference in the spreads. This implies that the 
+#very close-value means could be falsely interpreted fairly easily unless the 
+#spread of the data was taken into account as well. Thus the scatter plot perhaps
+#presents some context that would be important to consider when looking at mean similarity.
